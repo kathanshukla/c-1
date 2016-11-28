756f6c4d17a517ead65864fc732401aded61216a
Sam Stephens <sam@growthintel.com>
2016-11-28 12:00:07 +0000

    Updated detab

--- detab.c
+++ detab.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #define TABSTOP 4
 
-void output_replaced_tab() {
-  for (int i = 0; i < TABSTOP; i++) {
-    putchar(' ');
-  }
+int column = 0;
+
+void output_char(char c) {
+  putchar(c);
+  if (c == '\n')
+    column = 0;
+  else
+    column++;
 }
 
 
 int main() {
   char c;
+
   while ((c = getchar()) != EOF) {
     if (c == '\t') {
-      output_replaced_tab();
-    } else putchar(c);
+      int cols_to_next_tabstop = column % TABSTOP;
+      for (int i = 0; i < cols_to_next_tabstop; i++) {
+        output_char(' ');
+      }
+    } else
+      output_char(c);
   }
   return 0;
 }
