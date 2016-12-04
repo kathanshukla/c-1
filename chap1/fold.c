1e135bcaffc6bf27d40f92e53517e41d258efc1c
Sam Stephens <sam@growthintel.com>
2016-12-04 19:08:23 +0000

    Fold program implementation

---
+++ chap1/fold.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#define MAXLINE 10000
+#define LINE_WRAP_LENGTH 5
+
+
+int get_line(char line[], int maxline);
+void print_range(char line[], int start, int end);
+int find_line_break_index(char line[], int start, int end);
+
+int main() {
+  char line[MAXLINE];
+  int len, start, end;
+
+  while ((len = get_line(line, MAXLINE)) > 0) {
+    start = 0;
+    end = len;
+    if (len > LINE_WRAP_LENGTH) {
+      end = find_line_break_index(line, start, LINE_WRAP_LENGTH);
+      print_range(line, start, end);
+      print_range(line, end, len); // Assumes needs to wrap only once
+    } else {
+      print_range(line, start, end);
+    }
+  }
+}
+
+int get_line(char s[], int lim) {
+  int c, i;
+
+  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; i++)
+    s[i] = c;
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void print_range(char line[], int position, int end) {
+  while (position < end) {
+    putchar(line[position]);
+    position++;
+  }
+  if (line[end-1] != '\n')
+    putchar('\n');
+}
+
+int find_line_break_index(char line[], int start, int position) {
+  int break_char_ix = position;
+  while (position != start) {
+    if (line[position] == ' ') {
+      break_char_ix = position;
+      break;
+    }
+    position--;
+  }
+  return break_char_ix;
+}
