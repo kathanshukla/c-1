8b9711965f3e778945422a802d00511b26770827
Sam Stephens <sam@growthintel.com>
2017-01-09 13:55:53 +0000

    Added getfloat (currently not working) and updated makefile

--- chap5/get_float.c
+++ chap5/get_float.c
@@ -4,40 +4,17 @@
 
 
 int main() {
-  int n, array[SIZE], getint(int *);
+  int n, getfloat(double *);
+  double array[SIZE];
 
-  for (n = 0; n < SIZE && getint(&array[n]) != 0 && getint(&array[n]) != EOF; n++)
-    printf("%d\n", array[n]);
+  for (n = 0; n < SIZE && getfloat(&array[n]) != 0 && getfloat(&array[n]) != EOF; n++)
+    printf("%f\n", array[n]);
 }
 
 int getch(void);
 void ungetch(int);
 
-int getint(int *pn) {
-  int c, sign;
-
-  while (isspace(c = getch()))
-    ;
-  if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
-    printf("%c is not a digit\n", c);
-    ungetch(c);
-    return 0;
-  }
-  sign = (c == '-') ? -1 : 1;
-  if (c == '+' || c == '-')
-    c = getch();
-  if (!isdigit(c)) {
-    ungetch(c);
-    return 0;
-  }
-  for (*pn = 0; isdigit(c); c = getch())
-    *pn = 10 * *pn + (c - '0');
-  *pn *= sign;
-  if (c != EOF)
-    ungetch(c);
-  return c;
-}
-
+// Figure out why this doesnt work
 int getfloat(double *pn) {
   int c, sign, decimal_places;
 
@@ -63,7 +40,7 @@ int getfloat(double *pn) {
       decimal_places++;
       *pn = 10 * *pn + (c - '0');
     }
-    *pn /= decimal_places;
+    *pn /= 10 * decimal_places;
   }
   *pn *= sign;
   if (c != EOF)
