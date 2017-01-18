f1457a88676fd5fdd6762ef7b2c078ae1254b287
Sam Stephens <sam@growthintel.com>
2017-01-18 13:51:18 +0000

    Added non-working str_end

--- chap5/strcat.c
+++ chap5/strcat.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int str_concat(char *, char *);
+int str_end(char *, char *);
 
 int main() {
-  char s[100] = "s";
-  char t[] = "OK - longer this time";
-  str_concat(s, t);
-  printf("%s\n", s);
+  char s[100] = "string with the end";
+  char t[] = "the end";
+  int end_retval;
+
+  end_retval = str_end(s, t);
+  printf("%d\n", end_retval);
 }
 
 int str_concat(char *s, char *t) {
@@ -16,4 +19,16 @@ int str_concat(char *s, char *t) {
   while ((*s++ = *t++) != '\0')
     ;
   return 0;
+}
+
+int str_end(char *s, char *t) {
+  char *t_start = t;
+
+  while (*s++ != '\0')
+    ;
+  while (*t++ != '\0')
+    ;
+  while ((*s-- == *t--) && (t > t_start))
+    ;
+  return (t >= t_start);
 }
