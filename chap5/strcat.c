73268e36c83d3cfbfa85a3767a43c5c764057447
Sam Stephens <sam@growthintel.com>
2017-01-23 13:11:32 +0000

    Make str_end function work consistently

--- chap5/strcat.c
+++ chap5/strcat.c
@@ -5,7 +5,7 @@ int str_end(char *, char *);
 
 int main() {
   char s[100] = "string with the end";
-  char t[] = "the end";
+  char t[] = "";
   int end_retval;
 
   end_retval = str_end(s, t);
@@ -21,14 +21,23 @@ int str_concat(char *s, char *t) {
   return 0;
 }
 
+// str_end - returns 1 if second string argument occurs at end of first string
+// if t is empty string, returns 1
 int str_end(char *s, char *t) {
   char *t_start = t;
 
-  while (*s++ != '\0')
+  if (*t == '\0') {
+    return 1;
+  } else if (*s == '\0') {
+    // s is empty string but t is not
+    return 0;
+  }
+
+  while (*++s != '\0') // Doesnt deal with empty strings
     ;
-  while (*t++ != '\0')
+  while (*++t != '\0') // Doesnt deal with empty strings
     ;
-  while ((*s-- == *t--) && (t > t_start))
+  while ((*--s == *--t) && (t >= t_start))
     ;
-  return (t >= t_start);
+  return (t == t_start - 1);
 }
