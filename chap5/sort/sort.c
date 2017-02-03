f0181923d16b6305ad8a697348bd61c8b7e7c6b7
Sam Stephens <sam@growthintel.com>
2017-02-03 13:57:29 +0000

    Added case ignore to sort

--- chap5/sort/sort.c
+++ chap5/sort/sort.c
@@ -15,8 +15,9 @@ int numcmp(const char *, const char *);
 int main(int argc, char *argv[])
 {
   int nlines;
-  int numeric = 0, reversed = 0;
+  int numeric = 0, reversed = 0, casefold = 0;
   char c;
+  int (*compfunc)(void*, void*);
 
   while (--argc > 0 && (*++argv)[0] == '-') {
     while ((c = *++argv[0])) {
@@ -27,6 +28,9 @@ int main(int argc, char *argv[])
         case 'r':
           reversed = 1;
           break;
+        case 'f':
+          casefold = 1;
+          break;
         default:
           printf("Unrecognised option %c\n", c);
           break;
@@ -34,14 +38,18 @@ int main(int argc, char *argv[])
     }
   }
 
+  // Below - casts function to a pointer to a  function that takes two void pointers
+  if (numeric) {
+    compfunc = (int (*)(void*, void*))numcmp;
+  } else if (casefold) {
+    compfunc = (int (*)(void*, void*))strcasecmp;
+  } else {
+    compfunc = (int (*)(void*, void*))strcmp;
+  }
+
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    quicksort((void **) lineptr, 0, nlines-1, reversed,
-          (int (*)(void*, void*))(numeric ? numcmp : strcmp)
-          /* Two casts going on here
-          First: Casts lineptr as pointer to void pointer
-          Second: Casts function to a pointer to a  function that takes two void pointers
-          */
-    );
+    quicksort((void **) lineptr, 0, nlines-1, reversed, compfunc);
+              // lineptr cast as pointer to void pointer
     writelines(lineptr, nlines);
     return 0;
   } else {
