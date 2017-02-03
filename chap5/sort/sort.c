3c2320fc1729b1510bb12902961598042d67f5fd
Sam Stephens <sam@growthintel.com>
2017-02-03 13:23:02 +0000

    Added test fixture and reverse option

--- chap5/sort/sort.c
+++ chap5/sort/sort.c
@@ -7,7 +7,7 @@ char *lineptr[MAXLINES];
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
-void quicksort(void *lineptr[], int left, int right,
+void quicksort(void *lineptr[], int left, int right, int reveresed,
                int (*comp)(void *, void *));
 
 int numcmp(const char *, const char *);
@@ -15,12 +15,25 @@ int numcmp(const char *, const char *);
 int main(int argc, char *argv[])
 {
   int nlines;
-  int numeric = 0;
+  int numeric = 0, reversed = 0;
 
-  if (argc > 1 && strcmp(argv[1], "-n"))
-    numeric = 1;
+  while (--argc > 0 && (*++argv)[0] == '-') {
+    switch ((*argv)[1]) {
+      case 'n':
+        numeric = 1;
+        break;
+      case 'r':
+        reversed = 1;
+        break;
+      default:
+        printf("Unrecognised option %c\n", (*argv)[1]);
+        break;
+    }
+  }
+  // if (argc > 1 && strcmp(argv[1], "-n"))
+  //   numeric = 1;
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    quicksort((void **) lineptr, 0, nlines-1,
+    quicksort((void **) lineptr, 0, nlines-1, reversed,
           (int (*)(void*, void*))(numeric ? numcmp : strcmp)
           /* Two casts going on here
           First: Casts lineptr as pointer to void pointer
@@ -37,9 +50,9 @@ int main(int argc, char *argv[])
 
 #include <stdlib.h>
 
-void quicksort(void *v[], int left, int right, int (*comp)(void *, void *))
+void quicksort(void *v[], int left, int right, int reversed, int (*comp)(void *, void *))
 {
-  int i, last;
+  int i, last, rev_modifier = -2 * reversed + 1;
   void swap(void *v[], int, int);
 
   if (left >= right)
@@ -47,12 +60,12 @@ void quicksort(void *v[], int left, int right, int (*comp)(void *, void *))
   swap(v, left, (left + right) / 2);
   last = left;
   for (i = left + 1; i <= right; i++) {
-    if ((*comp)(v[i], v[left]) < 0)
+    if ((*comp)(v[i], v[left]) * rev_modifier < 0)
       swap(v, ++last, i);
   }
   swap(v, left, last);
-  quicksort(v, left, last-1, comp);
-  quicksort(v, last+1, right, comp);
+  quicksort(v, left, last-1, reversed, comp);
+  quicksort(v, last+1, right, reversed, comp);
 }
 
 void swap(void *v[], int i, int j)
