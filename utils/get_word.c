c22791ecab3aa229ab2f4a89fe55aa9e65384a6c
Sam Stephens <sam@growthintel.com>
2017-02-17 13:27:17 +0000

    Moved btree linked list

--- utils/get_word.c
+++ utils/get_word.c
@@ -1,8 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int is_valid_var_char(char c);
-int is_valid_var_start_char(char c);
+int is_skippable(char c);
 
 int get_word(char *word, int lim)
 {
@@ -10,19 +9,13 @@ int get_word(char *word, int lim)
   void ungetch(int);
   char *w = word;
 
-  while (isspace(c = getch()))
+  while (is_skippable(c = getch()))
     ;
   if (c != EOF) {
     *w++ = c;
   }
-  if (c == '#')
-    ; // Do nothing
-  else if (!is_valid_var_start_char(c)) {
-    *w = '\0';
-    return c;
-  }
   for ( ; --lim > 0; w++) {
-    if (!is_valid_var_char(*w = getch())) {
+    if (is_skippable(*w = getch())) {
       ungetch(*w);
       break;
     }
@@ -31,12 +24,10 @@ int get_word(char *word, int lim)
   return word[0];
 }
 
-int is_valid_var_char(char c)
+int is_skippable(char c)
 {
-  return (isalnum(c) || (c == '_'));
-}
-
-int is_valid_var_start_char(char c)
-{
-  return (isalpha(c) || (c == '_'));
+  return (isspace(c) || (c == ',') || (c == '(') || (c == ')') ||
+          (c == '"') || (c == '\'') || (c == '.') || (c == ';') ||
+          (c == '[') || (c == ']') || (c == '{') || (c == '}') ||
+          (c == '!') || (c == '?') || (c == '`'));
 }
